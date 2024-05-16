e4b8dcde0f3f4a37bcd81ee9698ec8b084b0dab1
Bilel3 <30528888+Bilel3@users.noreply.github.com>
2024-05-16 09:58:58 +0100

    Update purge.sh

--- purge.sh
+++ purge.sh
@@ -3,6 +3,9 @@
 # Define the parent directory
 parent_directory="/path/to/parent/directory"
 
+# Define the archive directory
+archive_directory="/path/to/archive/directory"
+
 # Define the patterns to match files
 weekly_pattern="*weekly*"
 monthly_pattern="*monthly*"
@@ -13,8 +16,8 @@ weekly_max_age=$((3 * 7 * 24 * 60 * 60)) # 3 weeks in seconds
 # Define the maximum age for monthly files (in seconds)
 monthly_max_age=$((30 * 24 * 60 * 60)) # 1 month in seconds
 
-# Delete weekly files older than 3 weeks
-find "$parent_directory" -type f -name "$weekly_pattern" -mtime +21 -exec rm {} \;
+# Tar weekly files older than 3 weeks
+find "$parent_directory" -type f -name "$weekly_pattern" -mtime +21 -exec tar -cvzf "$archive_directory/weekly_$(date +%Y-%m-%d).tar.gz" {} +
 
-# Delete monthly files older than 1 month
-find "$parent_directory" -type f -name "$monthly_pattern" -mtime +30 -exec rm {} \;
+# Tar monthly files older than 1 month
+find "$parent_directory" -type f -name "$monthly_pattern" -mtime +30 -exec tar -cvzf "$archive_directory/monthly_$(date +%Y-%m-%d).tar.gz" {} +
